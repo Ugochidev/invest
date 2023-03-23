60f6bd806ee964304148905bb46ff2390ad0029c
Ugochidev <ugochukwuchioma16@gmail.com>
2023-03-24 00:01:12 +0100

    .

--- src/config/databases/postgresql.ts
+++ src/config/databases/postgresql.ts
@@ -1,6 +1,7 @@
-// import "reflect-metadata";
+import "reflect-metadata";
 import { DataSource } from "typeorm";
-// import { User } from "../../modules/users/models/entities/User";
+import User from "../../modules/users/models/entities/User";
+import Profile from "../../modules/users/models/entities/Profile";
 import environment from "../environment";
 
 // Create a new DataSource instance with the database connection details
@@ -13,9 +14,9 @@ const AppDataSource = new DataSource({
   database: environment.databasePg,
   synchronize: true,
   logging: false,
-  entities: ["./src/modules/**/models/entities/*.ts"],
-  migrations: [/*...*/],
-  subscribers: ["invest"],
+  entities: [User, Profile],
+  migrations: ["./src/migrations/*.ts"],
+  applicationName: "invest",
   migrationsTableName: "migrations",
 });
 
@@ -25,5 +26,5 @@ AppDataSource.initialize()
     console.log("POSTGRESQL connected");
   })
   .catch((error) => console.log(error));
-  
+
 export default AppDataSource;
