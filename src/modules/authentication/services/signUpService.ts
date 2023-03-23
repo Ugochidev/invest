f05b2a6b6df500ab16eaf0e91f84e620dd6d7a8f
Ugochidev <ugochukwuchioma16@gmail.com>
2023-03-23 15:42:32 +0100

    added sign UP

--- src/modules/authentication/services/signUpService.ts
+++ src/modules/authentication/services/signUpService.ts
@@ -1,7 +1,16 @@
 import bcrypt from "bcrypt";
+import UserRepository from "../../users/models/repositories/UserRepository";
+import Cache from "../../../shared/services/Redis";
 
 
+class RegisterUserService {
+  private userRepository: UserRepository;
+  private cache: Cache;
 
-class RegisterUserService{
-
+  constructor() {
+    this.userRepository = new UserRepository();
+    this.cache = new Cache();
+  }
 }
+
+export default RegisterUserService;
